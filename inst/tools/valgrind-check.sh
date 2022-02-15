ae472ed834b02e5563c18e84578921109e44088e
gitedric <cedric.noel@univ-lorraine.fr>
2022-02-15 17:34:32 +0100

    valgrind check 2

--- inst/tools/valgrind-check.sh
+++ inst/tools/valgrind-check.sh
@@ -10,18 +10,18 @@ mkdir -p valgrind-check
 # Note that we use -l and --install-args="--clean" in order to not have in the
 # valgrind-check directory (uploaded as artifact in GH actions) some large
 # directories and files produced as part of R CMD check
-docker run --rm -v $(pwd):/rTRNG wch1/r-debug bash -c ' \
-  RDvalgrind -e "install.packages(\"remotes\"); remotes::install_deps(\"rTRNG\", dependencies = TRUE)" \
-  && RDvalgrind CMD build /rTRNG \
+docker run --rm -v $(pwd):/trajeR wch1/r-debug bash -c ' \
+  RDvalgrind -e "install.packages(\"remotes\"); remotes::install_deps(\"trajeR\", dependencies = TRUE)" \
+  && RDvalgrind CMD build /trajeR \
   && mkdir valgrind-check-lib \
   && RDvalgrind -d "valgrind --track-origins=yes" CMD check \
-    -o /rTRNG/valgrind-check \
+    -o /trajeR/valgrind-check \
     -l valgrind-check-lib \
     --install-args="--clean" \
     --use-valgrind --no-stop-on-test-error \
-    $(ls rTRNG_*.tar.gz) \
+    $(ls trajeR_*.tar.gz) \
   '
 
 # Collect valgrind "ERROR SUMMARY" lines from all log files
-grep -ri "ERROR SUMMARY" valgrind-check/rTRNG.Rcheck --exclude-dir=*pkg_src > valgrind-check/valgrind-summary
+grep -ri "ERROR SUMMARY" valgrind-check/trajeR.Rcheck --exclude-dir=*pkg_src > valgrind-check/valgrind-summary
 cat valgrind-check/valgrind-summary
